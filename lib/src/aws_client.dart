9af3314bcea6f27cac2b66a621a98f5aee59d37a
Danilo Martins <danilomartins18@hotmail.com>
2024-11-14 12:51:49 -0300

    removing unused import

--- lib/src/aws_client.dart
+++ lib/src/aws_client.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be found
 // in the LICENSE file.
 
-import 'dart:developer';
-
 import 'package:http_client/console.dart';
 import 'package:http_client/http_client.dart' as http;
 
